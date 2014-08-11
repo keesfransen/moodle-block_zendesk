c29472ee0fb34c5515f25772a6cfde358a5c9bc1
keesfransen <kees@ited.co.nz>
2014-08-12 10:41:51 +1200

    removed specialisation and moved stuff around general tidy up

--- block_zendesk.php
+++ block_zendesk.php
@@ -7,10 +7,6 @@ class block_zendesk extends block_base {
         $this->title = get_string('pluginname', $this->blockname);
     }
     
-    public function specialization() {
-        $this->get_config();
-    }
-    
     public function has_config() {
         return true;
     }
@@ -43,8 +39,21 @@ class block_zendesk extends block_base {
         return $attributes;
     }
     
-    function get_required_javascript() {
-        parent::get_required_javascript();
+    public function get_content() {
+        
+        if (!isloggedin()) {
+            $this->content = NULL;
+            return $this->content;
+        }
+        
+        if ($this->content !== null) {
+          return $this->content;
+        }
+        $this->get_config();
+        
+        $this->content         =  new stdClass;
+        $this->content->text   = '';
+        $this->content->footer = '';
         
         // check the permissions and only require JS if we have the capability and 
         // if user is not a siteadmin only require if we have allow_teacher_role 
@@ -58,29 +67,27 @@ class block_zendesk extends block_base {
         } else if (is_siteadmin($this->config->user->id) && $hascapability) {
             $this->page->requires->yui_module('moodle-block_zendesk-zendesk', 'M.block_zendesk.zendesk.init', array($this->config));
         }
-    }
-    
-    public function get_content() {
-        
-        if ($this->content !== null) {
-          return $this->content;
-        }
-        
-        $this->content         =  new stdClass;
-        $this->content->text   = '';
-        $this->content->footer = '';
         
         // If we are in editing mode and the user is a siteadmin show the config (can view in the source)
         if ($this->page->user_is_editing() && is_siteadmin($this->config->user->id)) {
-            $this->content->text = $this->get_config(true);
+
+            $this->content->text = html_writer::start_tag('dl');
+            foreach ($this->config as $key => $value) {
+            
+                if (is_string($value)) {
+                    $this->content->text .= html_writer::tag('dt' , get_string('tab_'.$key, get_class($this)) . ': ');
+                    $this->content->text .= html_writer::tag('dd', $value );
+                }
+            }
+            $this->content->text .= html_writer::end_tag('dl');
+            
+            // $this->content->text = $output;
         }
         
         return $this->content;
     }
     
-    
     // Setup the configuration to pass to the YUI module
-    // This is executed in specialization() so that there is one configuration for all instances.
     private function get_config($print = false) {
         
         global $USER;
@@ -101,21 +108,6 @@ class block_zendesk extends block_base {
         $config->user->email     = $USER->email;
         $config->user->firstname = $USER->firstname;
         $config->user->lastname  = $USER->lastname;
-        
-        // Format the $config object so that it can be displayed nicely in the block
-        if ($print) {
-            $output = html_writer::start_tag('dl');
-            foreach ($config as $key => $value) {
-                
-                if (is_string($value)) {
-                    $output .= html_writer::tag('dt' , get_string('tab_'.$key, get_class($this)) . ': ');
-                    $output .= html_writer::tag('dd', $value );
-                }
-            }
-            $output .= html_writer::end_tag('dl');
-            
-            return $output;
-        }
         
         return $this->config = $config;
     }
